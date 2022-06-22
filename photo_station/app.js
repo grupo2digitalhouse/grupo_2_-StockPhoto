5af5a51bb22d2da0fae97fd8d1423183855fe216
AntezanaAlejandro <antezana1299@gmail.com>
2022-06-22 02:29:05 -0300

    22-6

--- photo_station/app.js
+++ photo_station/app.js
@@ -25,8 +25,6 @@ app.get('/productPayment', (req, res)=>{
 })
 
 
-//Ruteo LOCAL
-app.listen(3001, ()=> console.log("Servidor escuchando en puerto 3001"));
 //ruteo HEROKU
 app.set('puerto',process.env.PORT || 3000);
 app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
