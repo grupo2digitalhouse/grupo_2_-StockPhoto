b04e26cdb130b3d2575c30d8611817c4987647b4
MarianFz <marian.13f@gmail.com>
2022-10-23 23:02:06 -0300

    agregue cambios tontos

--- photo_station/src/controllers/registerController.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const controller = {
-    home: (req, res) => {
-        res.render(path.resolve(__dirname,'../views/users/register.ejs'));
-    }
-};
-
-module.exports = controller;
--- photo_station/src/routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router();
-const registerController = require('../controllers/registerController');
-
-/*REGISTER*/
-router.get('/register', registerController.home);
-module.exports = router;
